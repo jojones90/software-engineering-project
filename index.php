760f7a271fab8523f18fa62b4dfb926dd38bae50
Andrew Pirkle <andrewcpirkle@gmail.com>
2016-09-15 23:27:29 -0400

    started lumberjack github repo
    created the repo and already added the ccn for bootstrap for both
    javascript and css. Also added jquery link. tested to make sure input
    from text file can be read. Waiting for Tayler’s UI before I can test
    more.

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+  'session_start'
+ ?>
+ <html>
+ <head>
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ </head>
+
+ <body>
+<?php
+ function inputLumber($file){
+   foreach ($file as $lumber) {
+     echo $lumber."<br/>";
+   }
+ }
+ $reader = file('lumber_test.txt');
+ inputLumber($reader);
+
+ ?>
+
+ </body>
+ </html>
